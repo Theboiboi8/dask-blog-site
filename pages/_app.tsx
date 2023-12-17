1cf87b11b694b2a1814c2fc28eaa0a68944724f7
Theboiboi8 <kamentanchev@gmail.com>
2023-12-17 21:02:41 +0200

    Vercel Speed Insights
    Signed-off-by: Theboiboi8 <kamentanchev@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import { AppProps } from 'next/app'
+import {AppProps} from 'next/app'
 import '../styles/index.css'
-import { Analytics } from '@vercel/analytics/react';
+import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+	        <SpeedInsights />
+        </>
+    );
 }
