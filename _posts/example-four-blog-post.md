3b1744bf2462cfc9c018bdecab22263e148dbe7e
Theboiboi8 <kamentanchev@gmail.com>
2023-12-17 20:38:15 +0200

    Make the site

--- _posts/example-four-blog-post.md
+++ _posts/example-four-blog-post.md
@@ -1,13 +1,13 @@
 ---
-title: 'Example Blog Post'
+title: 'And Another One Even'
 excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.'
-coverImage: '/assets/blog/preview/cover.jpg'
-date: '2023-12-17T05:35:07.322Z'
+coverImage: '/assets/blog/dynamic-routing/cover.jpg'
+date: '2023-12-15T05:35:07.322Z'
 author:
   name: Kamela
   picture: '/assets/blog/authors/kamela.jpg'
 ogImage:
-  url: '/assets/blog/preview/cover.jpg'
+  url: '/assets/blog/hello-world/cover.jpg'
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.
--- components/meta.tsx
+++ components/meta.tsx
@@ -6,35 +6,25 @@ const Meta = () => {
     <Head>
       <link
         rel="apple-touch-icon"
-        sizes="180x180"
+        sizes="1440x1440"
         href="/favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/favicon/favicon.svg"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
+        href="/favicon/favicon.svg"
+        color="#ed1c24"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <link rel="shortcut icon" href="/favicon/favicon.svg" />
+      <meta name="msapplication-TileColor" content="#ed1c24" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Web for the masses.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
