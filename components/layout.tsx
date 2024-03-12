8fb3b73d7fe2e761d731ca99c1fabbbace1cec95
Kamela <kamentanchev@gmail.com>
2024-03-12 13:29:29 +0200

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,6 +11,13 @@ const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
+<script type="text/javascript">
+      async function main(){
+        let pyodide = await loadPyodide();
+        console.log(pyodide.runPython("1 + 2"));
+      }
+      main();
+    </script>
       <div className="min-h-screen">
           {
              // <Alert preview={preview} />
